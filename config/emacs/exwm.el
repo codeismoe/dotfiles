9a73ae259d2912d5fc6a7d0460dcd60c82394abb
Peter Steidel <psteidelprogramming@gmail.com>
2021-01-18 12:34:29 -0500

    Move main init file to literate org

--- config/emacs/exwm.el
+++ config/emacs/exwm.el
@@ -5,6 +5,10 @@
         (start-process-shell-command
          "xrandr" nil "xrandr --output DVI-D-0 --mode 1920x1080 --pos 2560x1440 --output HDMI-0 --mode 1920x1080 --pos 320x0 --output DP-0 --primary --mode 2560x1440 --pos 0x1080 --rotate normal")))
 (exwm-randr-enable)
+
+(require 'exwm-systemtray)
+(exwm-systemtray-enable)
+
 (require 'exwm)
 (require 'exwm-config)
 (exwm-config-default)
